af2f0e3ab61bc04fbca91fc155122f4c04f4b61b
BrunaBaria <bruna.baria.m@gmail.com>
2024-01-02 16:23:52 -0300

    update resolução do desafio

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,26 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    //Implementado
+    public class Nokia : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria): base(numero, modelo, imei, memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+        }
+
+        public string Numero { get; }
+        public string Modelo { get; }
+        public string Imei { get; }
+        public int Memoria { get; }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"{nomeApp}");
+        }
     }
 }
--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,15 +2,66 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
+        private string numero;
+
+        public string GetNumero()
+        {
+            return numero;
+        }
+
+        public void SetNumero(string value)
+        {
+            numero = value;
+        }
+
+        private string modelo;
+
+        private string GetModelo()
+        {
+            return modelo;
+        }
+
+        private void SetModelo(string value)
+        {
+            modelo = value;
+        }
+
+        private string imei;
+
+        private string GetImei()
+        {
+            return imei;
+        }
+
+        private void SetImei(string value)
+        {
+            imei = value;
+        }
+
+        private int memoria;
+
+        private int GetMemoria()
+        {
+            return memoria;
+        }
+
+        private void SetMemoria(int value)
+        {
+            memoria = value;
+        }
+
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
-            Numero = numero;
+            SetNumero(numero);
+            SetModelo(modelo);
+            SetImei(imei);
+            SetMemoria(memoria);
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
+        
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
